94a0df7f66b001ea2c0c84ec5894caccc69676ed
David H. Angarita <davidangarita.sistemas@gmail.com>
2022-01-02 19:16:59 -0500

    Update DisponibilidadRecursoRouterTest.java

--- src/test/java/co/com/sofka/bibliotecaReactiva/Routers/Recurso/DisponibilidadRecursoRouterTest.java
+++ src/test/java/co/com/sofka/bibliotecaReactiva/Routers/Recurso/DisponibilidadRecursoRouterTest.java
@@ -1,7 +1,62 @@
 package co.com.sofka.bibliotecaReactiva.Routers.Recurso;
 
+import co.com.sofka.bibliotecaReactiva.Collections.Recurso;
+import co.com.sofka.bibliotecaReactiva.Mappers.RecursoMapper;
+import co.com.sofka.bibliotecaReactiva.Repositories.RepositorioRecurso;
+import co.com.sofka.bibliotecaReactiva.UseCases.Recurso.UseCaseDisponibilidadRecurso;
+import co.com.sofka.bibliotecaReactiva.Utils.Area;
+import co.com.sofka.bibliotecaReactiva.Utils.Tipo;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@WebFluxTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {DisponibilidadRecursoRouter.class, UseCaseDisponibilidadRecurso.class, RecursoMapper.class})
 class DisponibilidadRecursoRouterTest {
+    @MockBean
+    private RepositorioRecurso repositorio;
 
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void DisponibilidadRecursoTest(){
+        Recurso recurso1 = new Recurso();
+        recurso1.setId("123");
+        recurso1.setArea(Area.FANTASIA);
+        recurso1.setDisponible(true);
+        recurso1.setTipo(Tipo.LIBRO);
+        recurso1.setNombre("Harry Potter");
+        recurso1.setFecha(LocalDate.now());
+
+        Mono<Recurso> recursoMono = Mono.just(recurso1);
+
+        when(repositorio.findById(recurso1.getId())).thenReturn(recursoMono);
+
+
+        webTestClient.get()
+                .uri("/recursos/disponibilidad/123")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .value(userResponse -> {
+                            Assertions.assertThat(userResponse.equals("El recurso está disppnible"));
+                        }
+                );
+        Mockito.verify(repositorio,Mockito.times(1)).findById("123");
+    }
 }
